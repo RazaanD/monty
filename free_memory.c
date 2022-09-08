76988d305a4598712823a1d2909714ab6d3bdd63
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-09-08 22:10:17 +0200

    Update free_memory.c

--- free_memory.c
+++ free_memory.c
@@ -1,37 +1,58 @@
 #include "monty.h"
-
 /**
- * free_stack - frees a stack_t list
- * @head: head of linked list
- */
-
-void free_stack(stack_t *head)
+* execute - executes the opcode
+* @stack: head linked list - stack
+* @counter: line_counter
+* @file: poiner to monty file
+* @content: line content
+* Return: no return
+*/
+int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {
-	stack_t *current;
-	stack_t *next;
+	instruction_t opst[] = {
+				{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
+				{"pop", f_pop},
+				{"swap", f_swap},
+				{"add", f_add},
+				{"nop", f_nop},
+				{"sub", f_sub},
+				{"div", f_div},
+				{"mul", f_mul},
+				{"mod", f_mod},
+				{"pchar", f_pchar},
+				{"pstr", f_pstr},
+				{"rotl", f_rotl},
+				{"rotr", f_rotr},
+				{"queue", f_queue},
+				{"stack", f_stack},
+				{NULL, NULL}
+				};
+	
+	unsigned int i = 0;
+	char *op;
 
-	current = head;
-
-	while (current != NULL)
+	op = strtok(content, " \n\t");
+	
+	if (op && op[0] == '#')
+		return (0);
+	
+	bus.arg = strtok(NULL, " \n\t");
+	
+	while (opst[i].opcode && op)
 	{
-		next = current->next;
-		free(current);
-		current = next;
-	}
-}
-
-/**
- * free_lines - frees a line_t list
- * @lines: hpointer
- */
-void free_lines(line_t *lines)
-{
-	int i = 0;
-
-	while ((lines + i)->content != NULL)
-	{
-		free((lines + i)->content);
+		if (strcmp(op, opst[i].opcode) == 0)
+		{	opst[i].f(stack, counter);
+			return (0);
+		}
 		i++;
 	}
-	free(lines);
+	
+	if (op && opst[i].opcode == NULL)
+	{ fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
+		fclose(file);
+		free(content);
+		free_stack(*stack);
+		exit(EXIT_FAILURE); 
+	}
+	return (1);
 }
