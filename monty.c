06dbec23ebd6b76ccde7af66b0ef5f79e56102d0
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-09-08 22:20:49 +0200

    Update monty.c

--- monty.c
+++ monty.c
@@ -1,58 +1,53 @@
 #include "monty.h"
-#define BUFSIZE 64
 
-char *operand;
+bus_t bus = {NULL, NULL, NULL, 0};
 
 /**
- * main - monty interpreter
- * @argc: int
- * @argv: list of arguments
- * Return: nothing
- */
-int main(int argc, char const *argv[])
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
+* Return: 0 on success
+*/
+
+int main(int argc, char *argv[])
 {
-	line_t *lines;
-	char **line;
-	int line_number;
-	stack_t *stack;
 	char *content;
-	void (*func)(stack_t**, unsigned int);
+	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
 
-	stack = NULL;
-
-	if (argc == 1)
+	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	lines = textfile_to_array(argv[1]);
-	if (lines == NULL)
-		return (0);
-
-	line_number = 0;
-	while ((lines + line_number)->content != NULL)
+	
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	
+	if (!file)
 	{
-		content = (lines + line_number)->content;
-		line = split_line(content);
-		operand = line[1];
-
-		func = get_op_func(line[0]);
-		if (func == NULL)
-		{
-			/*TODO: Refactor: Edit more efifcient way to free memory on exit*/
-			fprintf(stderr, "L%d: unknown instruction %s\n", line_number + 1, line[0]);
-			free(line);
-			free_stack(stack);
-			free_lines(lines);
-			exit(EXIT_FAILURE);
-		}
-
-		func(&stack, line_number + 1);
-		free(line);
-		line_number++;
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
 	}
-
+	
+	while (read_line > 0)
+	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		counter++;
+		
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
+		free(content);
+	}
+	
 	free_stack(stack);
-	free_lines(lines);
-	return (0);
+	fclose(file);
+return (0);
 }
